7a6fe9b48a800363c2269907d5e9f78e704e227e
Dmitri Popov <dmpop@linux.com>
2011-07-09 18:21:44 +0200

    First commit

---
+++ pygmynote.py
@@ -0,0 +1,325 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Pygmynote is a command-line tool for storing and managing heterogeneous personal data.
+Pygmynote is written in Python and uses a SQLite database as its back-end.
+
+Thanks to Luis Cabrera Sauco for the SQLite and i18 support.
+
+Referencias:
+http://www.devshed.com/index2.php?option=content&task=view&id=845&pop=1&hide_ads=1&page=0&hide_js=1
+http://gcubo.org/documentacion/recetas/gettextpython/ ó http://www.gcubo.org/documentacion/recetas/gettextpython/
+http://www.miguev.net/misc/PyGettextMicroComo.html
+
+i18:
+~~~~
+pygettext -k_ -o pygmynote.po pygmynote.py
+msgfmt pygmynote.po -o pygmynote.mo
+mkdir -p es/LC_MESSAGES
+mv pygmynote.mo es/LC_MESSAGES/pygmynote.mo
+mv pygmynote.po es/LC_MESSAGES/pygmynote.po
+
+test_i18:
+~~~~~~~~~
+$ LANGUAGE=es python pygmynote.py
+"""
+
+__author__ = 'Dmitri Popov [dmpop@linux.com]'
+__copyright__ = 'Copyright 2011 Dmitri Popov'
+__license__ = 'GPLv3'
+__version__ = '0.7.7'
+__URL__ = 'http://www.github.com/dmpop'
+
+import sys
+import datetime
+import os
+import time
+import calendar
+import gettext
+
+DEBUG = False
+
+DOMAIN = 'pygmynote'
+
+try:
+	TRANSLATION = gettext.translation(DOMAIN, '.')
+	_ = TRANSLATION.ugettext
+except IOError:
+	_ = gettext.gettext
+
+try:
+	import sqlite3 as sqlite
+	if DEBUG == True:
+		print 'Use sqlite3, with python %s' % sys.version
+except ImportError:
+	from pysqlite2 import dbapi2 as sqlite
+	if DEBUG == True:
+		print 'Use pysqlite2, with python %s' % sys.version
+
+DB = 'pygmynote.db'
+ENC = 'utf-8'
+
+if os.path.exists(DB):
+	if DEBUG == True:
+		print 'The database already exists.'
+	CREATE = False
+else:
+	if DEBUG == False:
+		print 'Creating a new database.'
+	CREATE = True
+
+try:
+	if DEBUG == False:
+		conn = sqlite.connect(DB)
+	else:
+		conn = sqlite.connect(DB, timeout=0.5, encoding=ENC)
+	cursor = conn.cursor()
+except:
+	sys.exit('Connection to the SQLite database failed!')
+
+today = time.strftime('%Y-%m-%d')
+command = ''
+
+print """\033[1;32m
+           ( 0)>
+          (( *))
+            ||
+==========="=="=============
+Pygmynote is ready. Pile up!
+============================\033[1;m\n"""
+
+def escapechar(sel):
+	sel=sel.replace("\'", "\''")
+	sel=sel.replace("\"", "\\\"")
+	return sel
+
+
+if CREATE == True:
+	CREATE_SQL = \
+		"CREATE TABLE notes (\
+		id INTEGER PRIMARY KEY UNIQUE NOT NULL,\
+		note VARCHAR(1024),\
+		file BLOB,\
+		due DATE,\
+		type VARCHAR(3),\
+		ext VARCHAR(3),\
+		tags VARCHAR(256));"
+	cursor.execute(CREATE_SQL)
+	conn.commit()
+
+print 'Today\'s deadlines:'
+cursor.execute ("SELECT due, id, note, tags FROM notes WHERE due = '"+today+"' AND type <> 'H' ORDER BY id ASC")
+rows = cursor.fetchall()
+for row in rows:
+	print '\n%s -- \033[1;32m%s\033[1;m %s \033[1;30m[%s]\033[1;m' % (row[0], row[1], row[2], row[3])
+
+while command != 'q':
+
+	try:
+
+		command = raw_input('\n:')
+
+		if command == 'help':
+			print """
+===================
+Pygmynote commands:
+===================
+
+i	Insert new record
+f	Insert new record with an attachment
+s	Save attachment
+n	Search records by note
+t	Search records by tag
+a	Show active records
+h	Show hidden records
+u	Update record
+tl	Show tasks
+ext	Show records with attachments
+cal	Show calendar
+w	Export records as CSV file
+d	Delete record by its ID
+q	Quit"""
+
+		elif command == 'i':
+
+# Insert new record
+
+			notetext = escapechar(raw_input('Note: '))
+			notetags = escapechar(raw_input('Tags: '))
+			notedue = raw_input('Due date (yyyy-mm-dd). Press Enter to skip: ')
+			notetype = "A"
+			sqlquery = \
+				"INSERT INTO notes (note, due, tags, type) VALUES ('%s', '%s', '%s', '%s')"\
+				% (notetext, notedue, notetags, notetype)
+			cursor.execute(sqlquery)
+			conn.commit()
+			print '\nRecord has been added.'
+		elif command == 'f':
+
+# Insert new record with file
+
+			notetext = escapechar(raw_input('Note: '))
+			notetags = escapechar(raw_input('Tags: '))
+			notefile = escapechar(raw_input('Enter path to file: '))
+			notetype="A"
+			f=open(notefile, 'rb')
+			ablob = f.read()
+			f.close()
+			cursor.execute("INSERT INTO notes (note, tags, type, ext, file) VALUES('"+notetext+"', '"+notetags+"', '"+notetype+"', '" +notefile[-3:]+"', ?)", [sqlite.Binary(ablob)])
+			conn.commit()
+			print '\nRecord has been added.'
+		elif command == 's':
+
+# Save file
+
+			recid = raw_input('Record id: ')
+			outputfile=raw_input('Specify full path and file name (e.g., /home/user/loremipsum.odt): ')
+			f=open(outputfile, 'wb')
+			cursor.execute ("SELECT file FROM notes WHERE id='" + recid + "'")
+			ablob = cursor.fetchone()
+			f.write(ablob[0])
+			f.close()
+			cursor.close()
+			conn.commit()
+			print '\nFile has been saved.'
+		elif command == 'n':
+
+# Search records by note
+
+			notetext = raw_input('Search notes for: ')
+			cursor.execute("SELECT id, note, tags FROM notes WHERE note LIKE '%"
+							+ notetext + "%'ORDER BY id ASC")
+			rows = cursor.fetchall()
+			print '\n*********'
+			for row in rows:
+				print '\n\033[1;32m%s\033[1;m %s \033[1;30m[%s]\033[1;m' % (row[0], row[1], row[2])
+			print '\n*********'
+		elif command == 't':
+
+# Search records by tag
+
+			searchtag = raw_input ('Search by tag: ')
+			cursor.execute("SELECT id, note, tags FROM notes WHERE tags LIKE '%"
+							+ searchtag + "%' AND type='A' ORDER BY id ASC")
+			rows = cursor.fetchall()
+			print '\n*********'
+			for row in rows:
+				print '\n\033[1;32m%s\033[1;m %s \033[1;30m[%s]\033[1;m' % (row[0], row[1], row[2])
+			print '\n*********'
+		elif command == 'a':
+
+# Show active records
+
+			cursor.execute("SELECT id, note, tags FROM notes WHERE type='A' ORDER BY id ASC")
+			rows = cursor.fetchall()
+			print '\n*********'
+			for row in rows:
+				print '\n\033[1;32m%s\033[1;m %s \033[1;30m[%s]\033[1;m' % (row[0], row[1], row[2])
+			print '\n*********'
+		elif command == 'h':
+
+# Show hidden records
+
+			cursor.execute("SELECT id, note, tags FROM notes WHERE type='H' ORDER BY id ASC")
+			rows = cursor.fetchall()
+			print '\n*********'
+			for row in rows:
+				print '\n\033[1;32m%s\033[1;m %s \033[1;30m[%s]\033[1;m' % (row[0], row[1], row[2])
+			print '\n*********'
+		elif command == 'u':
+
+# Update note
+
+			recid = raw_input('Record id: ')
+			notetype = raw_input('Update note (n), tags (t), due date (d), or mark as hidden (h): ')
+			if notetype == 'n':
+				cursor.execute ("SELECT id, note FROM notes WHERE id='" + recid + "'")
+				row = cursor.fetchone()
+				print 'Current contents: %s' % row[1]
+				noteupdate = raw_input('Note: ')
+				sqlstr = escapechar(noteupdate)
+				cursor.execute("UPDATE notes SET note='" + sqlstr
+								+ "' WHERE id='" + recid + "'")
+			elif notetype == 't':
+				tagupdate = raw_input('Tags: ')
+				sqlstr = escapechar(tagupdate)
+				cursor.execute("UPDATE notes SET tags='" + sqlstr
+								+ "' WHERE id='" + recid + "'")
+			elif notetype == 'd':
+				dueupdate = raw_input('Due date: ')
+				cursor.execute("UPDATE notes SET due='" + dueupdate
+								+ "' WHERE id='" + recid + "'")
+			else:
+				notetype = 'H'
+				cursor.execute("UPDATE notes SET type='" + notetype
+								+ "' WHERE id='" + recid + "'")
+			conn.commit()
+			print '\nRecord has been updated.'
+		elif command == 'tl':
+
+# Show tasks
+
+			cursor.execute ("SELECT due, id, note, tags FROM notes WHERE due <> '' AND type = 'A' ORDER BY due ASC")
+			rows = cursor.fetchall()
+			print '\n*********'
+			for row in rows:
+				print '\n%s -- \033[1;32m%s\033[1;m %s \033[1;30m[%s]\033[1;m' % (row[0], row[1], row[2], row[3])
+			print '\n*********'
+		elif command == 'ext':
+
+# Show records with attachments
+
+			cursor.execute("SELECT id, note, tags, ext FROM notes WHERE ext <> 'None' AND type='A' ORDER BY id ASC")
+			rows = cursor.fetchall()
+			print '\n*********'
+			for row in rows:
+				print '\n\033[1;32m%s\033[1;m %s \033[1;30m[%s]\033[1;m \033[1;43m%s\033[1;m' % (row[0], row[1], row[2], row[3])
+			print '\n*********'
+		elif command == 'cal':
+
+# Show calendar
+
+			whichmonth = input('Month (1-12): ')
+			date = time.localtime()[:2]
+			print '\n'
+			calendar.prmonth(date[0], whichmonth)
+			print
+		elif command == 'd':
+
+# Delete note by its ID
+
+			recid = raw_input('Delete note ID: ')
+			cursor.execute("DELETE FROM notes WHERE ID='" + recid + "'")
+			print '\nRecord has been deleted.'
+			conn.commit()
+		elif command == 'w':
+
+# Save all notes as pygmynote.txt
+
+			cursor.execute("SELECT id, note, tags, due FROM notes ORDER BY id ASC")
+			rows = cursor.fetchall()
+			if os.path.exists('pygmynote.txt'):
+				os.remove('pygmynote.txt')
+			for row in rows:
+				filename = 'pygmynote.txt'
+				file = open(filename, 'a')
+				file.write('%s\t%s\t[%s]\t%s\n' % (row[0], row[1], row[2], row[3]))
+				file.close()
+			print '\nRecords have been saved in the pygmynote.txt file.'
+
+	except:
+		print '\n\n\033[1;31mSomething went wrong. Try again.\033[1;m'
+
+		continue
+
+print """\033[1;31m
+      ( 0)>
+     (( *))
+       ||
+======"=="======
+Have a nice day!
+================\033[1;m\n"""
+
+cursor.close()
+conn.close()
